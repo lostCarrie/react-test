6aeb3e17e4fb840d5844750f32fd405ed4752877
Carrie <imr.mu@icloud.com>
2020-01-10 17:54:11 +0800

    [add] Add new redux test

---
+++ src/pages/huzi_redux/App.ts
@@ -0,0 +1,18 @@
+
+interface appContent {
+    text: string;
+    color: string;
+}
+
+const appState = {
+    title: {
+        text: 'react 小书标题',
+        color: 'red',
+    },
+    content: {
+        text: 'react 小书内容',
+        color: 'black',
+    }
+}
+
+function 
